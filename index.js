b2d170f2eb957f0badc305da80b2cc8b2e9ab9bb
SOFIA DE AZEREDO LEIRIA <02970346001@senacrs.edu.br>
2024-03-06 15:54:25 -0300

    exercicio

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-futebol nao ta com nada
+// Criar algoritmo que verifica a idade e valide a necessidade de uma pessoa de votar
+// Não possui idade
+// Obrigatório 
+// Facultativo
+
+// Declarações
+const nome = "Getúlio";
+let idade = 18;
+
+// Desenvolvimento
+// menor que 16 não vota
+// igual a 16/17 voto facultativo
+// maior que 18 obrigatório
+// a partir de 65 facultativo
+
+if(idade < 16){
+    console.log(nome + " não tem idade para votar!");
+} else if(idade >= 16 && idade < 18 || idade >= 65){
+    console.log(nome + " seu voto é facultativo!");
+} else{
+    console.log(nome + " seu voto é obrigatório!");
+}
+
+// Informar o nome e a classificação de obrigatoriedade de votar
